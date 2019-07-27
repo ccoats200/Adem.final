37aa22016c44e33f15a9bc44aadffc7b3963b972
ccoats200 <ccoats200@gmail.com>
2019-07-27 13:15:42 -0400

    controllers

---
+++ Adem/Controllers/ProductInfoVC.swift
@@ -0,0 +1,24 @@
+//
+//  ProductInfoVC.swift
+//  Adem
+//
+//  Created by Coleman Coats on 7/27/19.
+//  Copyright © 2019 Coleman Coats. All rights reserved.
+//
+
+import UIKit
+
+class productInfoVC: UIViewController {
+    
+    //Reuse ID
+    let itemViewControllerDesign = "product"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
